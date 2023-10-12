3db2b70099a8911554e950d52495d05fd2188ff0
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2023-10-12 12:07:21 +0200

    Fixing conditions

--- modules/terraform-juju-grafana-agent-k8s/main.tf
+++ modules/terraform-juju-grafana-agent-k8s/main.tf
@@ -13,7 +13,7 @@ resource "juju_application" "grafana-agent" {
 }
 
 resource "juju_integration" "prometheus-remote-write" {
-  count = var.metrics_remote_write_offer_url ? 1 : 0
+  count = var.metrics_remote_write_offer_url != "" ? 1 : 0
   model = var.model_name
 
   application {
@@ -27,7 +27,7 @@ resource "juju_integration" "prometheus-remote-write" {
 }
 
 resource "juju_integration" "loki-logging" {
-  count = var.logging_offer_url ? 1 : 0
+  count = var.logging_offer_url != "" ? 1 : 0
   model = var.model_name
 
   application {
